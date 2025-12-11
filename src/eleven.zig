d353c6f0b5d0c9e38845d3d5d1e270e195c8d89d
oskar123456 <cph-oh82@cphbusiness.dk>
2025-12-11 17:44:07 +0100

    day 11

--- src/eleven.zig
+++ src/eleven.zig
@@ -28,26 +28,6 @@ fn first(arena: std.mem.Allocator) !void {
     print("\nTotal paths from \"you\" to \"out\": {}\n", .{res});
 }
 
-fn countPaths(arena: Allocator, graph: Graph, from: string, to: string) u64 {
-    var stack: List([]const u8) = .empty;
-    stack.append(arena, from) catch unreachable;
-
-    var sum: u64 = 0;
-    while (stack.items.len > 0) {
-        const dev = stack.swapRemove(0);
-        const outs_found = graph.get(dev);
-
-        if (common.strEql(dev, to)) {
-            sum += 1;
-        } else if (outs_found) |outs| {
-            for (outs.items) |o| {
-                stack.append(arena, o) catch unreachable;
-            }
-        }
-    }
-    return sum;
-}
-
 fn second(arena: std.mem.Allocator) !void {
     input.header(11, 2);
     const in = try input.get(11);
@@ -69,44 +49,24 @@ fn second(arena: std.mem.Allocator) !void {
     );
 }
 
-fn sortTopological(arena: Allocator, graph: Graph) []string {
-    var in_degrees: StringMap(u64) = .init(arena);
-    var nodes_top_sort: List(string) = .empty;
-    var queue: List(string) = .empty;
+fn countPaths(arena: Allocator, graph: Graph, from: string, to: string) u64 {
+    var stack: List([]const u8) = .empty;
+    stack.append(arena, from) catch unreachable;
 
-    var graph_it = graph.iterator();
-    while (graph_it.next()) |entry| {
-        const id = entry.key_ptr.*;
-        const neighbors = entry.value_ptr.*.items;
-        freqTryAdd(&in_degrees, id);
-        for (neighbors) |neighbor| {
-            freqIncr(&in_degrees, neighbor);
-        }
-    }
+    var sum: u64 = 0;
+    while (stack.items.len > 0) {
+        const dev = stack.swapRemove(0);
+        const outs_found = graph.get(dev);
 
-    var in_degrees_it = in_degrees.iterator();
-    while (in_degrees_it.next()) |entry| {
-        const id = entry.key_ptr.*;
-        const in_degree = entry.value_ptr.*;
-        if (in_degree == 0) {
-            queue.append(arena, id) catch unreachable;
-        }
-    }
-
-    while (queue.items.len > 0) {
-        const node = queue.swapRemove(0);
-        nodes_top_sort.append(arena, node) catch unreachable;
-        if (graph.get(node)) |neighbor_list| {
-            for (neighbor_list.items) |neighbor| {
-                freqDecr(&in_degrees, neighbor);
-                if (freqGet(&in_degrees, neighbor) == 0) {
-                    queue.append(arena, neighbor) catch unreachable;
-                }
+        if (common.strEql(dev, to)) {
+            sum += 1;
+        } else if (outs_found) |outs| {
+            for (outs.items) |o| {
+                stack.append(arena, o) catch unreachable;
             }
         }
     }
-
-    return nodes_top_sort.toOwnedSlice(arena) catch unreachable;
+    return sum;
 }
 
 fn calcPaths(
@@ -152,6 +112,73 @@ fn calcPaths(
     return if (scores.get(to)) |s| s else 0;
 }
 
+fn sortTopological(arena: Allocator, graph: Graph) []string {
+    var in_degrees: StringMap(u64) = .init(arena);
+    var nodes_top_sort: List(string) = .empty;
+    var queue: List(string) = .empty;
+
+    var graph_it = graph.iterator();
+    while (graph_it.next()) |entry| {
+        const id = entry.key_ptr.*;
+        const neighbors = entry.value_ptr.*.items;
+        freqTryAdd(&in_degrees, id);
+        for (neighbors) |neighbor| {
+            freqIncr(&in_degrees, neighbor);
+        }
+    }
+
+    var in_degrees_it = in_degrees.iterator();
+    while (in_degrees_it.next()) |entry| {
+        const id = entry.key_ptr.*;
+        const in_degree = entry.value_ptr.*;
+        if (in_degree == 0) {
+            queue.append(arena, id) catch unreachable;
+        }
+    }
+
+    while (queue.items.len > 0) {
+        const node = queue.swapRemove(0);
+        nodes_top_sort.append(arena, node) catch unreachable;
+        if (graph.get(node)) |neighbor_list| {
+            for (neighbor_list.items) |neighbor| {
+                freqDecr(&in_degrees, neighbor);
+                if (freqGet(&in_degrees, neighbor) == 0) {
+                    queue.append(arena, neighbor) catch unreachable;
+                }
+            }
+        }
+    }
+
+    return nodes_top_sort.toOwnedSlice(arena) catch unreachable;
+}
+
+fn parse(arena: std.mem.Allocator, in: string) !Graph {
+    var graph: Graph = .init(arena);
+
+    var input_it = std.mem.splitScalar(u8, in, '\n');
+    while (input_it.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
+
+        var line_it = std.mem.splitScalar(u8, line, ' ');
+        const word_first = (if (line_it.next()) |w| w else unreachable)[0..3];
+        std.debug.assert(word_first.len == 3);
+        var output_list: List(string) = .empty;
+        while (line_it.next()) |word| {
+            if (word.len == 0) {
+                continue;
+            }
+
+            try output_list.append(arena, word);
+        }
+
+        try graph.put(word_first, output_list);
+    }
+
+    return graph;
+}
+
 fn freqAdd(freqs: *StringMap(u64), key: string, to_add: u64) void {
     if (freqs.get(key)) |freq| {
         freqs.put(key, freq + to_add) catch unreachable;
@@ -189,31 +216,4 @@ fn freqTryAdd(freqs: *StringMap(u64), key: string) void {
     if (freqs.get(key) == null) {
         freqs.put(key, 0) catch unreachable;
     }
-}
-
-fn parse(arena: std.mem.Allocator, in: string) !Graph {
-    var graph: Graph = .init(arena);
-
-    var input_it = std.mem.splitScalar(u8, in, '\n');
-    while (input_it.next()) |line| {
-        if (line.len == 0) {
-            continue;
-        }
-
-        var line_it = std.mem.splitScalar(u8, line, ' ');
-        const word_first = (if (line_it.next()) |w| w else unreachable)[0..3];
-        std.debug.assert(word_first.len == 3);
-        var output_list: List(string) = .empty;
-        while (line_it.next()) |word| {
-            if (word.len == 0) {
-                continue;
-            }
-
-            try output_list.append(arena, word);
-        }
-
-        try graph.put(word_first, output_list);
-    }
-
-    return graph;
 }
